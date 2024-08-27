d92f451703c082b0b4928e3cb3f0dea2a71395aa
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-27 16:07:57 -0300

    Chapter 01: customizing how models are displayed

--- Blog Application/mysite/blog/admin.py
+++ Blog Application/mysite/blog/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Allows to set the field of the model is going to be displayed on admin list page
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    # Allows to filter the results by the fields included in the list_filter
+    list_filter = ['status', 'created', 'publish', 'author']
+    # Defines a list of searchable fields
+    search_fields = ['title', 'body']
+    # Tells to Django to populate the slug field with the input of the title field
+    prepopulated_fields = {'slug': ('title',)}
+    # Display a lookup widget to search for specific user
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
