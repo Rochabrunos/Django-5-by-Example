1983926b68f04cac46a578d5e8c280cfcd37c9d3
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-27 15:23:44 -0300

    Chapter 01: creating a superuser and accessing Django administration site

--- Blog Application/mysite/docker-entrypoint.sh
+++ Blog Application/mysite/docker-entrypoint.sh
@@ -3,4 +3,7 @@
 cd /usr/src/app/
 python manage.py makemigrations blog
 python manage.py migrate 
+
+# By default the password will be read from $DJANGO_SUPERUSER_PASSWORD environment variable
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 python manage.py runserver 0.0.0.0:8000
