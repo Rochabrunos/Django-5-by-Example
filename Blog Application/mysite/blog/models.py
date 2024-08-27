91f92ade858df91cd402da369e167a86b0f0b722
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-27 13:20:29 -0300

    Chapter 01: adding datetime fields

--- Blog Application/mysite/blog/models.py
+++ Blog Application/mysite/blog/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.db.models.functions import Now
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    publish = models.DateTimeField(db_default=Now)
+    # auto_now_add saves the date automatically when creating an object (tracks creation time)
+    created = models.DateTimeField(auto_now_add=True)
+    # auto_now will automatically update the date when saving an object (tracks last modification time)
+    updated = models.DateTimeField(auto_now=True)
 
     # Django uses this method to return a string with the human-readable representation of the object
     def __str__(self):
